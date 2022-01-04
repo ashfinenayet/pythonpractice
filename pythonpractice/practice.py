bb90be139076431d23a4c3ca6a59b045e17c752d
ashfinenayet <ashfin.enayet@gmail.com>
2022-01-04 15:12:43 -0500

    added some more changes

--- pythonpractice/practice.py
+++ pythonpractice/practice.py
@@ -1,16 +1,16 @@
 import calendar
 import pandas as pd
-df = pd.read_excel('modified.xlsx')
-# print(df.tail(3))
-# print(df.columns)
+df = pd.read_csv('tiktok.csv')
+df.drop_duplicates(inplace=True)
+# removes duplicates from original data frame
 #print(df[['City/State', 'Category', 'Country']])
 #df['year'] = (pd.DatetimeIndex(df['Date']).year)
 #df['month'] = (pd.DatetimeIndex(df['Date']).month)
-df['date'] = pd.to_datetime(df['Date'])
-df['Date'] = pd.to_datetime(df.Date, format='%m-%d-%Y')
+
 #print(df.loc[29, 'Category'])
 #print(df.sort_values(['Category', 'Amount'], ascending=[1,0]))
 #print(df.head(5))
 #df.reset_index(inplace=True)
 #df['month_name'] = df['month'].apply(lambda x: calendar.month_abbr[x])
 #df.to_excel('modified2.xlsx', index=False)
+df.to_excel('modifiedtiktok.xlsx', index=False)
